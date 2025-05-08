04361fcd670d138d56186d7b156de4402f71bb5c
examp1 <examp1.web@gmail.com>
2025-05-08 22:07:38 +0300

    svg fix 1

--- composables/useUtils.js
+++ composables/useUtils.js
@@ -3,7 +3,12 @@ import { MEDIA_PATH_PREFIX, FILE_PREFIX, BASE_URL } from "~/constants";
 export default function useUtils() {
     const getMediaPath = (path) => {
         if (!path) return "";
-        if (path.includes('jpg') || path.includes('png') || path.includes('webp')) {
+        if (
+            path.includes("jpg") ||
+            path.includes("png") ||
+            path.includes("webp") ||
+            path.includes("svg")
+        ) {
             return `${BASE_URL}/${MEDIA_PATH_PREFIX}${path}`;
         } else {
             return `${BASE_URL}/${FILE_PREFIX}${path}`;
