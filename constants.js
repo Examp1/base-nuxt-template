9a0ec50c7a97d3e5db7e90485df34482e0a0bd47
examp1 <examp1.web@gmail.com>
2025-05-09 18:08:54 +0300

    new bas url

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://blade-base.sidev.digital'
+export const BASE_URL = 'https://admin.sidev.digital'
 export const MEDIA_PATH_PREFIX = 'storage/media'
 // export const MEDIA_PATH_SVG_GIF = 'storage/media'
 export const FILE_PREFIX = 'storage/files'
