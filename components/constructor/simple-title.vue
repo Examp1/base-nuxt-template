989c80b4fbca158b1d6beaa76aa2cc0b43f33dcd
examp1 <examp1.web@gmail.com>
2025-06-12 20:32:28 +0300

    simple title

---
+++ components/constructor/simple-title.vue
@@ -0,0 +1,27 @@
+<script setup>
+import appButtons from "../common/app-buttons.vue";
+import appSectionTitle from "../common/app-section-title.vue";
+
+defineProps({
+    content: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div
+        :class="[`container-${content.container_size}`, content.text_position]"
+    >
+        <appSectionTitle
+            v-if="content.title"
+            :title="content.title"
+            :titleType="content.tag_variant"
+        ></appSectionTitle>
+        <appButtons v-if="content.btns" :btns="content.btns"></appButtons>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
